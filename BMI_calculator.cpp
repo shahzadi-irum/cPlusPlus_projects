12bd4a0cfb34b40b39b00d707dc9f5fab3fe4ef4
shahzadi-irum <109738580+shahzadi-irum@users.noreply.github.com>
2025-03-19 04:38:46 +0500

    Update BMI_calculator.cpp

--- BMI_calculator.cpp
+++ BMI_calculator.cpp
@@ -1,8 +1,4 @@
 how to create siomple BMI calculator in cPlusPlus
-// just create some two variables Heigth(h) and Weight(w), with float data types
-// Take them input from the user
-// put thse values in a fomrula which is : BMI = weight/height*height
-// print the resutlts to the user
 #include<iostream>
 #include<cmath>
 using namespace std;
